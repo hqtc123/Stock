9c8a1622bfb3cff62e77c37709d699eb8ac0f2fa
hqtc <814216440@qq.com>
2013-07-24 23:41:16 +0800

    a part of work , include database and a father json

--- src/com/hq/network/GetSortInfo.java
+++ src/com/hq/network/GetSortInfo.java
@@ -1,5 +1,13 @@
 package com.hq.network;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created with IntelliJ IDEA.
  * User: He Qing
@@ -9,4 +17,57 @@ package com.hq.network;
  */
 public class GetSortInfo {
     public static final String SOURCE = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodes";
+    String jsonString = "";
+
+    public GetSortInfo() {
+        init();
+    }
+
+    public void init() {
+        try {
+            URL url = new URL(SOURCE);
+            URLConnection context = url.openConnection();
+            InputStream in = context.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "gb2312"));
+            char[] cbuf = new char[1024];
+            int cnum = -1;
+            while ((cnum = br.read(cbuf)) != -1) {
+                jsonString += new String(cbuf, 0, cnum);
+            }
+
+            System.out.println(jsonString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void print() {
+        JSONArray startObj = JSONArray.fromString(jsonString);
+        JSONArray huS = JSONArray.fromObject(startObj.get(1));
+        JSONArray hus2 = JSONArray.fromObject(huS.get(0));
+
+        JSONArray fat = JSONArray.fromObject(hus2.get(1));
+
+        JSONArray sectionArray = JSONArray.fromObject(fat.get(1));
+        JSONArray sectionArray2 = JSONArray.fromObject(sectionArray.get(2));
+
+        JSONArray regionArray = JSONArray.fromObject(fat.get(2));
+        JSONArray regionArray2 = JSONArray.fromObject(regionArray.get(2));
+
+        JSONArray industryArray = JSONArray.fromObject(fat.get(3));
+        //for(industry*****)
+        System.out.println(fat.get(1));
+        System.out.println(fat.get(2));
+        System.out.println(fat.get(3));
+    }
+
+    public void addRegion() {
+
+    }
+
+    public static void main(String[] args) {
+        new GetSortInfo().print();
+    }
 }
--- src/com/hq/network/Price.java
+++ src/com/hq/network/Price.java
@@ -1,8 +1,11 @@
 package com.hq.network;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,47 +31,7 @@ public class Price {
     // 成交股票数量
     public long   total_number;
     // 成交金额
-    public double total_price;
-    // 买一要买的股数
-    public long      buyer_1_number;
-    // 买一报价
-    public double buyer_1_price;
-    // 买二要买的股数
-    public long      buyer_2_number;
-    // 买二报价
-    public double buyer_2_price;
-    // 买三要买的股数
-    public long      buyer_3_number;
-    // 买三报价
-    public double buyer_3_price;
-    // 买四要买的股数
-    public long      buyer_4_number;
-    // 买四报价
-    public double buyer_4_price;
-    // 买五要买的股数
-    public long      buyer_5_number;
-    // 买五报价
-    public double buyer_5_price;
-    // 卖一要卖的股数
-    public long      seller_1_number;
-    // 卖一报价
-    public double seller_1_price;
-    // 卖二要卖的股数
-    public long      seller_2_number;
-    // 卖二报价
-    public double seller_2_price;
-    // 卖三要卖的股数
-    public long      seller_3_number;
-    // 卖三报价
-    public double seller_3_price;
-    // 卖四要卖的股数
-    public long      seller_4_number;
-    // 卖四报价
-    public double seller_4_price;
-    // 卖五要卖的股数
-    public long      seller_5_number;
-    // 卖五报价
-    public double seller_5_price;
+
     // 日期
     public Long   time;
     private Price()
@@ -79,10 +42,12 @@ public class Price {
             String input = SOURCE+stock_num;
             String output = "";
             URL url = new URL(input);
-            InputStreamReader reader = new InputStreamReader(new BufferedInputStream(url.openStream()));
+            URLConnection context = url.openConnection();
+            InputStream in = context.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "gb2312"));
             char[] cbuf = new char[1024];
             int cnum = -1;
-            while((cnum=reader.read(cbuf))!=-1)
+            while((cnum=br.read(cbuf))!=-1)
             {
                 output += new String(cbuf, 0, cnum);
             }
@@ -98,27 +63,7 @@ public class Price {
             stockInfo.today_max_price = Double.valueOf(infos[4]);
             stockInfo.today_min_price = Double.valueOf(infos[5]);
             stockInfo.total_number = Long.valueOf(infos[8]);
-            stockInfo.total_price =  Double.valueOf(infos[9]);
-            stockInfo.buyer_1_number = Long.valueOf(infos[10]);
-            stockInfo.buyer_1_price = Double.valueOf(infos[11]);
-            stockInfo.buyer_2_number = Long.valueOf(infos[12]);
-            stockInfo.buyer_2_price = Double.valueOf(infos[13]);
-            stockInfo.buyer_3_number = Long.valueOf(infos[14]);
-            stockInfo.buyer_3_price = Double.valueOf(infos[15]);
-            stockInfo.buyer_4_number = Long.valueOf(infos[16]);
-            stockInfo.buyer_4_price = Double.valueOf(infos[17]);
-            stockInfo.buyer_5_number = Long.valueOf(infos[18]);
-            stockInfo.buyer_5_price = Double.valueOf(infos[19]);
-            stockInfo.seller_1_number = Long.valueOf(infos[20]);
-            stockInfo.seller_1_price = Double.valueOf(infos[21]);
-            stockInfo.seller_2_number = Long.valueOf(infos[22]);
-            stockInfo.seller_2_price = Double.valueOf(infos[23]);
-            stockInfo.seller_3_number = Long.valueOf(infos[24]);
-            stockInfo.seller_3_price = Double.valueOf(infos[25]);
-            stockInfo.seller_4_number = Long.valueOf(infos[26]);
-            stockInfo.seller_4_price = Double.valueOf(infos[27]);
-            stockInfo.seller_5_number = Long.valueOf(infos[28]);
-            stockInfo.seller_5_price = Double.valueOf(infos[29]);
+
             stockInfo.time = System.currentTimeMillis()/1000;
             return stockInfo;
         }catch(Exception e){
@@ -128,20 +73,11 @@ public class Price {
     }
     public void printStockInfo(){
         System.out.println(this.stock_name);
-        System.out.println("买五     " + this.buyer_1_number + " : " + this.buyer_1_price);
-        System.out.println("买四     " + this.buyer_2_number + " : " + this.buyer_2_price);
-        System.out.println("买三     " + this.buyer_3_number + " : " + this.buyer_3_price);
-        System.out.println("买二     " + this.buyer_4_number + " : " + this.buyer_4_price);
-        System.out.println("买一     " + this.buyer_5_number + " : " + this.buyer_5_price);
+
         System.out.println("\n当前价格     " + this.current_price+"\n");
-        System.out.println("卖一     " + this.seller_1_number + " : " + this.seller_1_price);
-        System.out.println("卖二     " + this.seller_2_number + " : " + this.seller_2_price);
-        System.out.println("卖三     " + this.seller_3_number + " : " + this.seller_3_price);
-        System.out.println("卖四     " + this.seller_4_number + " : " + this.seller_4_price);
-        System.out.println("卖五     " + this.seller_5_number + " : " + this.seller_5_price);
+
         System.out.println("时间     " + this.time);
         System.out.println("\n成交量：" + this.total_number);
-        System.out.println("成交金额：" + this.total_price);
 
     }
     public static void main(String[] args){
