f06f962cb1484a41ba49dd286b0c69796ffb8c7a
hqtc123 <hqdo@foxmail.com>
2016-04-21 10:35:06 +0800

    change stock to hqtc

--- static/js/route.js
+++ static/js/route.js
@@ -2,8 +2,8 @@
  * Created by Qing on 2015/12/6.
  */
 app.config(["$routeProvider", function ($routeProvider) {
-    $routeProvider.when("/about/", {
-        templateUrl: "/static/templates/about.html"
+    $routeProvider.when("/index/", {
+        templateUrl: "/static/templates/index.html"
     }).when("/companies", {
         templateUrl: "/static/templates/companies.html"
     }).when("/company/:code", {
@@ -14,5 +14,5 @@ app.config(["$routeProvider", function ($routeProvider) {
         templateUrl: "/static/templates/jplag.html"
     }).when("/allusion", {
         templateUrl: "/static/templates/allusion.html"
-    }).otherwise({redirectTo: "/companies/"})
+    }).otherwise({redirectTo: "/index/"})
 }]);
